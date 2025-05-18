84af254587ed76dd65371c013d7550cbf7ef0b6e
klee2187 <kkelly6@byui.edu>
2025-05-17 22:52:25 -0500

    worked on coolpics site

---
+++ coolpics/coolpics.js
@@ -0,0 +1,43 @@
+const menuButton = document.querySelector(".menu-button");
+function toggleMenu() {
+    const menu = document.querySelector("nav");
+    menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+function handleResize() {
+    const menu = document.querySelector("nav");
+    if (window.innerWidth > 1000) {
+        menu.classList.remove("hide");
+    }
+}
+
+handleResize();
+window.addEventListener("resize", handleResize);
+
+const dialog = document.createElement("dialog");
+dialog.classList.add("image-viewer");
+document.body.appendChild(dialog);
+
+function handleImageClick(event) {
+    const clickedImage = event.target.closest("img");
+    if(!clickedImage) return;
+
+    const srcParts = clickedImage.src.split("-");
+    const fullImageSrc = srcParts[0] + "-full.jpeg";
+
+    dialog.innerHTML = `<img src="${fullImageSrc}" alt="${clickedImage.alt}">
+        <button class='close-viewer'>X</button>`;
+
+    dialog.showModal();
+}
+
+const gallery = document.querySelector(".gallery");
+gallery.addEventListener("click", handleImageClick);
+
+dialog.addEventListener("click", (event) =>{
+    if (event.target.classList.contains("close-viewer") || event.target === dialog) {
+        dialog.close();
+    }
+ });
